68997610adcdab9394f3f9c3fd5dcf0e3d47dc31
Veríssimo Casas <Verissimo0073@gmail.com>
2022-09-13 23:09:29 -0400

    Stage 2/6 mobile - stop 1h32m

--- App.tsx
+++ App.tsx
@@ -1,8 +1,25 @@
 import { StatusBar } from 'react-native';
 
+import { 
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+ } from '@expo-google-fonts/inter';
+
+import { HOME } from './src/screens/Home/index';
 import { Background } from './src/components/Background';
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    useFonts,
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
   return (
     <Background>
       <StatusBar
@@ -10,7 +27,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-
+      {fontLoaded ?  <HOME />}
     </Background>
   );
 }
